12ac30459fcd48b141c01513af11c87d6973d3e6
BiswajitNathaSharma <122850529+BiswajitNathaSharma@users.noreply.github.com>
2023-12-15 10:21:07 +0530

    vite react installed

---
+++ 01_vite-react/src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div>
+    <center>
+      <h1>Chai aur Code || Biswajit Natha Sharma</h1> 
+      <h3>what i learn today</h3>
+    </center>
+
+    <ol>
+      <li>npm: node package manager <br /> npx: node package execute</li>
+      <li>How to install react.</li>
+      <li>How to instsll vite+react.</li>
+      <li>How to clean or maintain code.</li>
+      <li>How to "start" or "build" project using run command.</li>
+      <li>What is JSX.</li>
+    </ol>
+    </div>
+  )
+}
+
+export default App
