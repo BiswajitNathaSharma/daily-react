32ca4831f016c958fa375baef7629597d7d22978
BiswajitNathaSharma <122850529+BiswajitNathaSharma@users.noreply.github.com>
2024-01-27 18:30:00 +0530

    enter github username and see the magic

--- 09_theam-switcher/src/App.jsx
+++ 09_theam-switcher/src/App.jsx
@@ -1,59 +1,66 @@
-import { useState,useEffect } from 'react';
-import './App.css'
-import { ThemeProvider } from "./contexts/Theme";
+import React, { useState, useEffect} from 'react';
+import './App.css';
+import { ThemeProvider } from './contexts/Theme';
 import Card from './components/card';
 import ThemeBtn from './components/ThemeBtn';
 import UserInput from './components/UserInput';
 import useProfileInfo from './Hooks/useProfileInfo';
-function App() {
-  const [themeMode, setThemeMode] = useState("light")
-  const lightMode = () =>{
-    setThemeMode("light")
-  }
-  const darkMode = () =>{
-    setThemeMode("dark")
-  }
 
+function App() {
+  const [themeMode, setThemeMode] = useState('light');
+  const lightMode = () => {
+    setThemeMode('light');
+  };
+  const darkMode = () => {
+    setThemeMode('dark');
+  };
   useEffect(() => {
-  document.querySelector('html').classList.remove("light","dark")
-  document.querySelector('html').classList.add(themeMode)
-  }, [themeMode])
-  
-  const [username, setUsername] = useState("github")
-  const profileInfo = useProfileInfo(username)
-  
-  const handleSubmit = (submittedUrl) =>{
+    document.querySelector('html').classList.remove("light","dark")
+    document.querySelector('html').classList.add(themeMode)
+    }, [themeMode])
+  const [username, setUsername] = useState('github');
+  const { profileInfo, error } = useProfileInfo(username);
+
+  const handleSubmit = (submittedUrl) => {
     setUsername(submittedUrl);
   };
+
   return (
-
-<ThemeProvider value={{themeMode, darkMode, lightMode}}>
-<div className="flex flex-wrap min-h-screen items-center">
-                <div className="w-full">
-                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                        <ThemeBtn/>
-                    </div>
-                  <UserInput onUrlSubmit = {handleSubmit} />  
-
-                    <div className="w-full max-w-sm mx-auto">
-                        <Card 
-                        
-                        imgSrc={profileInfo.avatar_url}
-                        userName={profileInfo.login}
-                        gitlink={profileInfo.html_url}
-                        followers={profileInfo.followers}
-                        bio={profileInfo.bio}
-                        />
-                      
-                       
-                    </div>
-                </div>
+    <ThemeProvider value={{ themeMode, darkMode, lightMode }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
+          <UserInput onUrlSubmit={handleSubmit} />
+          {error ? (
+            <div className='w-full max-w-sm mx-auto'>
+                 <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <a href="/">
+                <img className="p-8 rounded-t-lg" src= "https://avatars.githubusercontent.com/u/9919?v=4" alt="user image" />
+            </a>
+            <div className="px-5 pb-5">
+                    <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                        {error}
+                    </h5> 
             </div>
-          
-            
-            </ThemeProvider>
-            
-  )
+        </div>
+            </div>
+          ) : (
+            <div className="w-full max-w-sm mx-auto">
+              <Card
+                imgSrc={profileInfo.avatar_url}
+                userName={profileInfo.login}
+                gitlink={profileInfo.html_url}
+                followers={profileInfo.followers}
+                bio={profileInfo.bio}
+              />
+            </div>
+          )}
+        </div>
+      </div>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
--- 09_theam-switcher/src/Hooks/useProfileInfo.js
+++ 09_theam-switcher/src/Hooks/useProfileInfo.js
@@ -1,27 +1,31 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from 'react';
 
-function useProfileInfo(username) {
-    const [ProfileInfo, setProfileInfo] = useState({})
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-              const response = await fetch(`https://api.github.com/users/${username}`);
-              
-              const data = await response.json();
-              setProfileInfo(data);
-              console.log(data)
-            } catch (error) {
-              
-              console.error('Error fetching data:', error);
-            }
-          };
-      
-          if (username) {
-            fetchData();
-          }
-    
-    }, [username])
-    
-    return ProfileInfo
-}
+const useProfileInfo = (username) => {
+  const [profileInfo, setProfileInfo] = useState({});
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchProfileInfo = async () => {
+      try {
+        const response = await fetch(`https://api.github.com/users/${username}`);
+        if (!response.ok) {
+          throw new Error('No such User on GitHub');
+        }
+        const data = await response.json();
+        setProfileInfo(data);
+        setError(null); // Reset error state
+      } catch (error) {
+        setError(error.message); // Set error message
+      }
+    };
+
+    fetchProfileInfo();
+
+   
+    return () => setProfileInfo({});
+  }, [username]);
+
+  return { profileInfo, error };
+};
+
 export default useProfileInfo;
