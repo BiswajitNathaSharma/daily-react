b4fe4369df3d7cc463cc292f6957893c457a4e6b
BiswajitNathaSharma <122850529+BiswajitNathaSharma@users.noreply.github.com>
2024-02-04 00:50:21 +0530

    on the way project

--- 09_theam-switcher/src/App.jsx
+++ 09_theam-switcher/src/App.jsx
@@ -30,7 +30,7 @@ function App() {
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-            <ThemeBtn />
+            <ThemeBtn themesMode={themeMode} />
           </div>
           <UserInput onUrlSubmit={handleSubmit} />
           {error ? (
--- 09_theam-switcher/src/components/ThemeBtn.jsx
+++ 09_theam-switcher/src/components/ThemeBtn.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import useTheme from '../contexts/Theme';
 
-export default function ThemeBtn() {
+export default function ThemeBtn({themesMode}) {
     
  const {themeMode, lightMode, darkMode} =   useTheme()
 const onChangeBtn = (e)=>{
@@ -20,7 +20,7 @@ const onChangeBtn = (e)=>{
                 checked= {themeMode==="dark"}
             />
             <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-            <span className="ml-3 text-sm font-medium text-gray-100">Toggle Theme</span>
+            <span className="ml-3 text-sm font-medium text-gray-100">{themesMode}</span>
         </label>
     );
 }
