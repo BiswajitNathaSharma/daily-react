1f54a0635292e396f99544aa7b917c2b950400a5
BiswajitNathaSharma <122850529+BiswajitNathaSharma@users.noreply.github.com>
2024-02-06 19:43:43 +0530

    learn react

--- 10TodoLocalStorage/src/App.jsx
+++ 10TodoLocalStorage/src/App.jsx
@@ -30,43 +30,53 @@ function App() {
 
   }, [todos])
   const [data, setData] = useState("")
+  const [img, setImg] = useState(null)
+
 
   useEffect(() => {
-      
+        
+    
+    
     fetch("https://api.freeapi.app/api/v1/public/quotes/quote/random")
     .then((res)=>(res.json()))
     .then((res)=>setData(res.data.content))
-
-  }, [])
+    
+    fetch('https://inspirobot.me/api?generate=true')
+    .then((res)=>(res.text()))
+    .then((res)=>(setImg(res)))
+  }, []);
 
 
     
     
   return (
-    <TodoProvider value={{todos, addTodo, deleteTodo, updateTodo ,toggleComplete}}>
-      <div className="bg-[#172842] min-h-screen py-8">
-                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
-                  <span className='text-xl text-center mb-8 mt-2'>{data}</span>
-                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
-                    <div className="mb-4">
-                        {/* Todo form goes here */} 
-                        <TodoForm/>
-                    </div>
-                    <div className="flex flex-wrap gap-y-3">
-                        {/*Loop and Add TodoItem here */}
+    <TodoProvider value={{ todos, addTodo, deleteTodo, updateTodo, toggleComplete }}>
+    <div className="flex flex-col lg:flex-row">
+      <div className="bg-[#172842] lg:min-h-screen py-8 flex-grow">
+        <div className="w-full max-w-2xl px-4 py-3 mx-auto text-white rounded-lg shadow-md">
+          <span className="mt-2 mb-8 text-xl text-center">{data}</span>
+          <h1 className="mt-2 mb-8 text-2xl font-bold text-center">Manage Your Todos</h1>
+          <div className="mb-4">
+            {/* Todo form goes here */}
+            <TodoForm />
+          </div>
+          <div className="flex flex-wrap gap-y-3">
+            {/* Loop and Add TodoItem here */}
+            {todos.map((todo) => (
+              <div key={todo.id} className="w-full">
+                <TodoItem todo={todo} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <img src={img} alt="motivational image" className="w-full h-auto max-w-full lg:w-auto lg:min-h-screen lg:max-w-none lg:ml-4" /> {/* Adjust image size and position */}
+    </div>
 
-                        {todos.map((todo)=> (
-                          <div
-                          key={todo.id}
-                          className='w-full'
-                          >
-                          <TodoItem todo={todo}/>
-                        </div>
-                        ))}
-                    </div>
-                </div>
-            </div>
-    </TodoProvider>
+  </TodoProvider>
+  
+
+
   )
 }
 
